2778878a26fcb6cd5588d267a5f5ad1ca4a9e56f
Zhao Wang <wangz2266@gmail.com>
2020-02-11 13:32:17 -0800

    modified spinner. add spinner2.js

---
+++ spinner2.js
@@ -0,0 +1,18 @@
+const spin = timelimit => {
+  let spinnerArr = ["|", "/", "-", "\\"];
+  let timeout = 0;
+  for (let i = 0; i < spinnerArr.length; i++ /* const char of spinnerArr */) {
+    let char = spinnerArr[i];
+    if (timeout > timelimit) {
+      return;
+    }
+    setTimeout(() => {
+      process.stdout.write(`\r${char}  `);
+    }, timeout);
+    timeout += 200;
+    if (i === spinnerArr.length - 1) {
+      i = 0;
+    }
+  }
+};
+spin(10000);
